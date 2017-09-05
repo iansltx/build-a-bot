025a57aa6a9d7b6e123910061c3283c4837c4ce4
Ian Littman <iansltx@gmail.com>
2017-09-05 18:45:50 -0500

    Add api.ai web hook, fallback handler
    No real action handlers yet; they'll be in the next commit!

--- bootstrap/services.php
+++ bootstrap/services.php
@@ -1,5 +1,13 @@
 <?php
 
+use iansltx\ApiAiBridge\Question, iansltx\ApiAiBridge\Answer;
+
 return function(\Pimple\Container $c, $env) {
-    // services will go here
+    $c[\iansltx\ApiAiBridge\Middleware\DoublePass::class] = function() {
+        return new \iansltx\ApiAiBridge\Middleware\DoublePass(\iansltx\ApiAiBridge\Router::buildFromClosureArray([
+            // no skills yet
+        ], function(Question $question, Answer $answer) : Answer {
+            return $answer->withSpeechAndText("Sorry, I don't know how to help you with that.");
+        }));
+    };
 };
