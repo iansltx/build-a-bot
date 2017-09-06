e5abb597009083f14c69d94b0c1131040abaa391
Ian Littman <iansltx@gmail.com>
2017-09-05 23:04:33 -0500

    Use a context to respond with the following event if the user wants it
    Achieved by setting a context on the first response, including the date
    of the event that we mentioned last. If the context has a date in a
    request, we use it rather than the normal date parameter, and pull the
    second event rather than the first.

--- src/ScheduleHandler.php
+++ src/ScheduleHandler.php
@@ -20,6 +20,14 @@ class ScheduleHandler implements HandlerInterface
     public function __invoke(Question $question, Answer $answer): Answer
     {
         date_default_timezone_set('America/Los_Angeles');
+
+        // if context date is set, use it
+        if (($contextDateParam = $question->getContextParam('schedule-followup', 'previous-date')) &&
+                ($contextDate = \DateTimeImmutable::createFromFormat(\DateTime::ATOM, $contextDateParam))) {
+            $filteredSchedule = $this->scheduleClient->getSchedule()->filterOutPast($contextDate);
+            return $answer->withSpeechAndText('Okay. The next event is ' . $filteredSchedule->second() . '.');
+        }
+
         $dateParam = $question->getParam('date', date('Y-m-d'));
 
         $filteredSchedule = $this->scheduleClient->getSchedule()->filterOutPast(
@@ -29,7 +37,17 @@ class ScheduleHandler implements HandlerInterface
         );
 
         try {
-            return $answer->withSpeechAndText('The next event is ' . $filteredSchedule->first() . '.');
+            $speech = 'The next event is ' . $filteredSchedule->first() . '.';
+
+            // if we have multiple events left in the conference, let the user ask for the next one
+            if (count($filteredSchedule) > 1) {
+                $speech .= ' Would you like me to tell you about the event after that one?';
+                $answer = $answer->withContext('schedule-followup', [
+                    'previous-date' => $filteredSchedule->first()->getStartsAt()->format(\DateTime::ATOM)
+                ], 1);
+            }
+
+            return $answer->withSpeechAndText($speech);
         } catch (NoMoreEventsException $e) {
             return $answer->withSpeechAndText("There are no Pacific Northwest PHP events on or after the " .
                 $after->format('jS') . '.');
