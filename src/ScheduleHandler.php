b16c48cb6dcc78e60701e397bd58c638b2039769
Ian Littman <iansltx@gmail.com>
2017-11-13 19:08:09 -0600

    Use a context to respond with the following event if the user wants it
    Achieved by setting a context on the first response, including the date
    of the event that we mentioned last, as well as a talk index since
    php[world] has multiple talks in the same slot. While there are events
    left in the schedule, we keep passing revised context values to allow
    for iterating through the event list.

--- src/ScheduleHandler.php
+++ src/ScheduleHandler.php
@@ -20,16 +20,39 @@ class ScheduleHandler implements HandlerInterface
     public function __invoke(Question $question, Answer $answer): Answer
     {
         date_default_timezone_set('America/New_York');
-        $dateParam = $question->getParam('date', date('Y-m-d'));
 
-        $filteredSchedule = $this->scheduleClient->getScheduleByEventId(6476)->filterOutBefore(
+        $nextTalkIndex = 0;
+        // if data param is set, use it
+        if ($dateParam = $question->getParam('date')) {
             $after = ($dateParam === date('Y-m-d') ?
                 new \DateTimeImmutable() :
-                \DateTimeImmutable::createFromFormat('Y-m-d H:i:s', $dateParam . ' 00:00:00'))
-        );
+                \DateTimeImmutable::createFromFormat('Y-m-d H:i:s', $dateParam . ' 00:00:00'));
+        // if context data is set, use it
+        } elseif (($contextDateParam = $question->getContextParam('schedule-followup', 'previous-date')) &&
+            ($after = \DateTimeImmutable::createFromFormat(\DateTime::ATOM, $contextDateParam))) {
+            $nextTalkIndex = $question->getContextParam('schedule-followup', 'talk-index') ?: 1;
+        } else {
+            $after = new \DateTimeImmutable();
+        }
+
+        $filteredSchedule = $this->scheduleClient->getScheduleByEventId(6476)->filterOutBefore($after);
 
         try {
-            return $answer->withSpeechAndText('The next event is ' . $filteredSchedule->first() . '.');
+            $speech = 'The next event is ' . $filteredSchedule[$nextTalkIndex] . '.';
+
+            // if we have multiple events left in the conference, let the user ask for the next one
+            if (count($filteredSchedule) > 1) {
+                $speech .= ' Would you like me to give you another event?';
+
+                // If the talk we're on is in a time slot after the specified time, reset the talk index counter,
+                // otherwise assume we're iterating through tracks at the same time and increment the talk index.
+                $answer = $answer->withContext('schedule-followup', [
+                    'previous-date' => $filteredSchedule[$nextTalkIndex]->getStartsAt()->format(\DateTime::ATOM),
+                    'talk-index' => $filteredSchedule[$nextTalkIndex]->getStartsAt() != $after ? 1 : ++$nextTalkIndex
+                ], 1);
+            }
+
+            return $answer->withSpeechAndText($speech);
         } catch (NoMoreEventsException $e) {
             return $answer->withSpeechAndText("There are no PHP World events on or after the " .
                 $after->format('jS') . '.');
